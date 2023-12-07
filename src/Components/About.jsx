0e997debbea66d1a6440e413e7bb8718d2311171
Shashank Kumar Srivastava <95871637+shashank0412@users.noreply.github.com>
2023-12-07 21:38:23 +0530

    change all

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "src/images/image1.jpg";
+import image from "src/images/2.jpg";
 
 const imageAltText = "Shashank Kumar Srivastava";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "src/images/image2.jpg";
+import image from "src/images/3.jpg";
 
 const imageAltText = "Shashank Kumar Srivastava";
 
