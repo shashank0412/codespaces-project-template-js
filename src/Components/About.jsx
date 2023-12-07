e04ea78d5040a5b42fe00690602a24a94a2460c3
Shashank Kumar Srivastava <95871637+shashank0412@users.noreply.github.com>
2023-12-07 20:56:24 +0530

    image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/image1.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Shashank Kumar Srivastava";
 
 /**
  * Sort description that expands on your title on the Home component.
